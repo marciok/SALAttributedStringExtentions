f3f99330f5972bf9d35d09a10928c2eb40377bd4
Marcio Klepacz <marcioklepacz@gmail.com>
2014-08-27 11:37:57 +0300

    Initial Commit

---
+++ SALAttributedStringExtentionsExample/SALViewController.m
@@ -0,0 +1,29 @@
+//
+//  SALViewController.m
+//  SALAttributedStringExtentionsExample
+//
+//  Created by Marcio Klepacz on 8/27/14.
+//  Copyright (c) 2014 Seeking Alpha. All rights reserved.
+//
+
+#import "SALViewController.h"
+
+@interface SALViewController ()
+
+@end
+
+@implementation SALViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
