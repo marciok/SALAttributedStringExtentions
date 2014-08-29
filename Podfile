f1d4d3676863b660931e6145e0f3f4ce282130eb
Marcio Klepacz <marcioklepacz@gmail.com>
2014-08-29 13:59:16 +0300

    Add unit tests

--- Podfile
+++ Podfile
@@ -2,3 +2,7 @@ platform :ios, "7.0"
 
 pod 'hpple', '~> 0.2'
 pod 'SDWebImage', '~> 3.7'
+
+target :SALAttributedStringExtentionsExampleTests do
+  pod 'Kiwi', '~> 2.3'
+end
---
+++ SALAttributedStringExtentionsExampleTests/SALAttributedStringExtensionsSpec.m
@@ -0,0 +1,43 @@
+//
+//  SALAttributedStringExtensionsSpec.m
+//  SALAttributedStringExtentionsExample
+//
+//  Created by Marcio Klepacz on 8/29/14.
+//  Copyright 2014 Seeking Alpha. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "SALAttributedStringExtensions.h"
+
+
+SPEC_BEGIN(SALAttributedStringExtensionsSpec)
+
+describe(@"SALAttributedStringExtensions", ^{
+    
+    __block SALAttributedStringExtensions *sut;
+    __block NSString *HTML;
+    
+    context(@"When converting a HTML to attributed string", ^{
+        
+        it(@"it should return attributedStringFromHTML with limitingImageHeight 0", ^{
+        
+            [[sut should] receive:@selector(attributedStringFromHTML:limitingImageHeight:) withArguments:HTML,0];
+        });
+        
+        context(@"when attributedStringFromHTML it's called out of the main thread", ^{
+            
+            beforeAll(^{
+                HTML = @"<img src='http://fakeimg.pl/500x300/000000/?text=%@' /><h2>Header Level 2</h2> <ol> <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li><li>Aliquam tincidunt mauris eu risus.</li> </ol> <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote> <h3>Header Level 3</h3> <ul> <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li> <li>Aliquam tincidunt mauris eu risus.</li></ul>";
+            });
+            
+            
+            xit(@"should throw an exception", ^{
+
+            });
+            
+        });
+    });
+
+});
+
+SPEC_END
